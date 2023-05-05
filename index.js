2b476f47aeaf474efaf4fe3a012fb543683b4f86
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-05-05 10:10:02 -0400

    Add prompts for grid rows and columns
    Couldn't use a form without server side logic and database.

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 let container = document.querySelector(".container");
+let gridLayoutBtn = document.querySelector("#gridLayoutBtn");
 
-//Grid
-//# of rows
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    //# of squares per row
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
-        //hover effect
-        square.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = "blue";
-            setTimeout(() => {
-                e.target.style.backgroundColor = "";
-            }, 200);
-        });
+gridLayoutBtn.addEventListener("click", gridLayout);
+
+function gridLayout() {
+    let numOfRows = prompt("Rows");
+    let numOfColumns = prompt("Columns");
+    //Grid
+    //# of rows
+    for (let i = 0; i < numOfRows; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        //# of columns
+        for (let j = 0; j < numOfColumns; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+            //hover effect
+            square.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = "blue";
+                setTimeout(() => {
+                    e.target.style.backgroundColor = "";
+                }, 500);
+            });
+        }
     }
 }
+
