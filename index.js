ee942db2a8101e69666717df6a0568fbdc30e23a
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-05-06 09:04:23 -0400

    Add a default grid and a clear/reset grid function

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 let container = document.querySelector(".gridContainer");
 let gridLayoutBtn = document.querySelector("#gridLayoutBtn");
 
+// let gridLayoutOptions = document.querySelectorAll('input[name="gridLayout"');
+// gridLayoutOptions.forEach((option) => {
+//     option.addEventListener('click', gridLayout)
+//     console.log(option)
+// })
+
 gridLayoutBtn.addEventListener("click", gridLayout);
 
+let numOfRows = 16;
+let numOfColumns = 16;
 
+for (let i = 0; i < numOfRows; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    //# of columns
+    for (let j = 0; j < numOfColumns; j++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        row.appendChild(square);
+        //hover effect
+        square.addEventListener('mouseover', getRandomColor);
+    }
+}
 
 function gridLayout() {
-    let numOfRows = getAndLimitRows() || 16;
-    let numOfColumns = getAndLimitColumns() || 16;
+    clearGrid();
+    numOfRows = getAndLimitRows() || 16;
+    numOfColumns = getAndLimitColumns() || 16;
     //Grid
     //# of rows
     for (let i = 0; i < numOfRows; i++) {
@@ -23,6 +45,11 @@ function gridLayout() {
             square.addEventListener('mouseover', getRandomColor);
         }
     }
+}
+
+function clearGrid() {
+    let rows = document.querySelectorAll(".row")
+    rows.forEach(row => { row.remove() });
 }
 
 function getAndLimitRows() {
@@ -53,3 +80,6 @@ function getRandomColor(e) {
     }, 500);
 }
 
+
+//Replace button with 3 options - radio buttons:
+//16x16, 32, 64
